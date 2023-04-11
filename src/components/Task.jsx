0f8cdfe49f1a7221a8ae8b65e95659c5b79b43bb
Folareen <sakawahab03@gmail.com>
2023-04-11 23:03:57 +0100

    done with mark as completed and delete actions

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,18 +1,109 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { useRef } from "react";
 import Category from "./Category";
 import { formatTime } from "../utils/formatTime";
+import { GestureHandlerRootView, Swipeable } from "react-native-gesture-handler";
+import { FontAwesome5, Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { db } from "../../firebase.config";
+import { useSelector } from "react-redux";
 
-const Task = ({ name, icon, duration: {startTime, endTime} }) => {
+const Task = ({ id, name, icon, duration: { startTime, endTime }, prevCard, setPrevCard, setSubmitting }) => {
   const { taskBox, taskNameBox, taskName, taskDuration } = styles;
-  return (
-    <View style={taskBox}>
-      <Category isInTask={true} name={icon} />
-      <View style={taskNameBox}>
-        <Text style={taskName}>{name}</Text>
-        <Text style={taskDuration}>{formatTime(startTime.toDate())} - {formatTime(endTime.toDate())}</Text>
+  const { user } = useSelector(state => state.auth)
+
+
+  const handleMarkAsDone = () => {
+    setSubmitting(true);
+    db.collection(user.user.uid).doc(id)
+      .update({
+        status: 'completed'
+      })
+      .then((docRef) => {
+        console.log(docRef);
+        alert("Task marked as completed!");
+      })
+      .catch((error) => {
+        alert(error);
+      })
+      .finally(() => setSubmitting(false));
+  };
+
+  const handleDelete = () => {
+    setSubmitting(true);
+    db.collection(user.user.uid).doc(id).delete()
+      .then((docRef) => {
+        console.log(docRef);
+        alert("Task deleted!");
+      })
+      .catch((error) => {
+        alert(error);
+      })
+      .finally(() => setSubmitting(false));
+  };
+
+  const cardRef = useRef(null)
+
+  const closePrev = () => {
+    if (prevCard != cardRef && prevCard?.current != null) {
+      prevCard.current.close()
+    }
+    setPrevCard(cardRef)
+  };
+
+  const renderRightActions = (progress, dragX) => {
+    return (
+      <View
+        style={{
+          margin: 0,
+          alignItems: "center",
+          flexDirection: "row",
+          width: 150,
+          paddingHorizontal: 10,
+          backgroundColor: '#242531',
+          marginVertical: 10,
+          borderTopRightRadius: 10,
+          borderBottomRightRadius: 10,
+          justifyContent: "space-between",
+        }}
+      >
+        <TouchableOpacity
+        >
+          <MaterialCommunityIcons name="pencil-outline" size={24} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleMarkAsDone}
+        >
+          <FontAwesome5 name="check-circle" size={24} color="green" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleDelete}
+        >
+          <Ionicons name="ios-trash-bin" size={24} color="red" />
+        </TouchableOpacity>
       </View>
-    </View>
+    );
+  };
+
+  return (
+    <GestureHandlerRootView>
+      
+      <Swipeable
+        ref={cardRef}
+        renderRightActions={(progress, dragX) =>
+          renderRightActions(progress, dragX)
+        }
+        onSwipeableOpen={closePrev}
+      >
+        <View style={taskBox}>
+          <Category isInTask={true} name={icon} />
+          <View style={taskNameBox}>
+            <Text style={taskName}>{name}</Text>
+            <Text style={taskDuration}>{formatTime(startTime.toDate())} - {formatTime(endTime.toDate())}</Text>
+          </View>
+        </View>
+
+      </Swipeable>
+    </GestureHandlerRootView>
   );
 };
 
